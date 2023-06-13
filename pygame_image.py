b72926e466ea323243db0a70c6aa71c932e9b3e3
cappy <c0a2205641@edu.teu.ac.jp>
2023-06-13 16:46:45 +0900

    こうかとん２

--- pygame_image.py
+++ pygame_image.py
@@ -7,20 +7,23 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img1=[bg_img,pg.transform.flip(bg_img,True,False)]*2
+
     kokaton_img=pg.image.load("ex01/fig/3.png")
-    kokaton_img1=pg.transform.flip(kokaton_img,True,False)
-    kokaton_img2=pg.transform.rotozoom(kokaton_img,10,1.0)
-    s=[kokaton_img1,kokaton_img2]
+    kokaton_img=pg.transform.flip(kokaton_img,True,False)
+    kokaton_img1=[kokaton_img,pg.transform.rotozoom(kokaton_img,10,1.0)]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+        
+        x = tmr%1600
+        for i in range(4):
+            screen.blit(bg_img1[i],[1600*i-x,0])
+        screen.blit(kokaton_img1[tmr%100//50], [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
-
+        clock.tick(100)
 
 if __name__ == "__main__":
     pg.init()
